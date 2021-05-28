3e250b02b5ec556bbb922124c23020ff2bcbf880
Raquel <raquel.yanez.gomez@accenture.com>
2021-05-28 22:27:39 +0200

    ho

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,65 @@ const User = require('../entities/user');
 const bcryptjs = require('bcryptjs');
 const { createToken} = require('../middlewares/jwt')
 const {googleValidator} = require('../middlewares/validator-google')
+//@desc PERMITE A LOS USUARIOS INICIAR SESION
+//@route POST /api/auth
+//@acces public
+/*
+const login = async (req,res) =>{
+
+    const { email, password } = req.body;
+
+    try {
+        //compruebo que existe el email
+        const user = await User.findOne( { email} );
+        if(user){
+        //verificar la contrasenha
+        const matchPassword = bcryptjs.compareSync( password , user.password);
+        if( !matchPassword ){
+            return res.status(400).json({ msg: 'User o Password incorrecta -password-'});
+        }
+        }
+        //generar el JWT
+        const token = await createToken(user.id);
+        res.json({msg:'Login success', user, token})
+
+    } catch (err) {
+        console.log(err)
+        res.status(401).json({
+            msg: 'User o Password incorrecta'
+        })
+        
+    }} 
+   */
+//@desc Los users con sesion iniciada pueden ver su perfil
+//@route GET /api/auth/:id
+//@acces private
+const getUserProfile= async (req,res) =>{
+
+    const { email, password } = req.body;
+
+    try {
+        //compruebo que existe el email
+        const user = await User.findOne( { email} );
+        if(user){
+        //verificar la contrasenha
+        const matchPassword = bcryptjs.compareSync( password , user.password);
+        if( !matchPassword ){
+            return res.status(400).json({ msg: 'User o Password incorrecta -password-'});
+        }
+        }
+        //generar el JWT
+        const token = await createToken(user.id);
+        res.json({msg:'Login success', user, token})
+    } catch (err) {
+        console.log(err)
+        res.status(401).json({
+            msg: 'User o Password incorrecta'
+        })
+        
+    }
+   
+} 
 
 //@desc Los users con sesion iniciada pueden ver su perfil y eliminarlo despues
 //@route GET /api/auth/:id
