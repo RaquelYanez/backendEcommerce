5336d88ea8d0c3e88582af3fa5193a714b07cdce
Raquel <raquel.yanez.gomez@accenture.com>
2021-06-01 20:46:55 +0200

    no message

--- src/routes/size.js
+++ src/routes/size.js
@@ -7,4 +7,9 @@ const router = Router();
 //Add new brand (ADMIN_ROLE) private(TOKEN)
 router.post('/',[ check('sizeName', 'El nombre es obligatorio').not().isEmpty()],addSize);
 
+const Size  = require('../entities/size');
+router.get('/', getSizeName = async function (req,res){
+    const sizes = await Size.find({})
+    res.status(200).json(sizes)
+})
 module.exports = router;
