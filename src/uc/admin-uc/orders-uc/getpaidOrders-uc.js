ebfd4a9b963237fc9530107d4ef1b96c07cd285f
Raquel <raquel.yanez.gomez@accenture.com>
2021-06-08 10:48:42 +0200

    deliveredAdmin

--- src/uc/admin-uc/orders-uc/getpaidOrders-uc.js
+++ src/uc/admin-uc/orders-uc/getpaidOrders-uc.js
@@ -1,7 +1,7 @@
 const Order = require('../../../entities/order');
 
 async function execute(){
-    const orders = await Order.find({isPaid:true})
+    const orders = await Order.find({$and:[{isPaid:true},{isDelivered:false}]})
         .populate('user', 'id name')
         .populate('orderProducts.product', '_id name image price sizeProduct category brand')
         return orders
