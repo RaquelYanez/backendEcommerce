bb77746786cc3064179afd6c6e2ab47681442d64
Raquel <raquel.yanez.gomez@accenture.com>
2021-06-09 15:29:58 +0200

    no message

--- src/middlewares/validate-hasOrder.js
+++ src/middlewares/validate-hasOrder.js
@@ -1,7 +1,7 @@
 const Order  = require('../entities/order');
 
 const isOrderValidator = async(id) => {
-    const isOrderValidator = await Order.find({user:id});
+    const isOrderValidator = await Order.find({order:id});
     console.log(isOrderValidator.length)
     if(isOrderValidator.length <= 0){
         throw new Error(`No se encuentra el ningun pedido ${id}`)
